3f6629b88309db412bc00d5f0b647007bcf6a94b
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-17 03:45:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(Array,Element) {
+  var newArray = Array.unshift(Element);
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(Array,Element) {
+  var Array = Array
+}
